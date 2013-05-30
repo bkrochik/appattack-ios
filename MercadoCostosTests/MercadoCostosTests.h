682a575b233be86085c72e801c61d5d69985dcd3
bkrochik <brian.krochik@mercadolibre.com>
2013-05-30 01:25:21 -0300

    Initial Commit

---
+++ MercadoCostosTests/MercadoCostosTests.h
@@ -0,0 +1,13 @@
+//
+//  MercadoCostosTests.h
+//  MercadoCostosTests
+//
+//  Created by Brian Krochik on 30/05/13.
+//  Copyright (c) 2013 Brian Krochik. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MercadoCostosTests : SenTestCase
+
+@end
